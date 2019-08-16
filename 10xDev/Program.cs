a5ee86c4650ea063c5cea04375c2c553e661dd61
Daniel <baharestani@gmail.com>
2019-08-16 22:52:22 +1000

    solution created

---
+++ 10xDev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10xDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
